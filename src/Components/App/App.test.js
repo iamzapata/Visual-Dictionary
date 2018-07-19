d23a21f7472a96f6ac5ff0839ceed021d990f9e3
Andres Zapata <me@azapata.io>
2018-07-18 20:38:18 -0500

    Add named export to <App /> for testing purposes. Update <App /> tests

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,32 +1,31 @@
 import React from "react"
 import { render } from "react-testing-library"
-import App from "./index.jsx"
+import { App } from "./index.jsx"
 
 describe("<App />", () => {
-  const store = {
-    subscribe: () => null,
-    dispatch: () => null,
-    getState: () => null
-  }
-  let props = { header: "Visual Dictionary", store }
+  const createProps = () => ({
+    searchWord: jest.fn(),
+    SearchBoxStore: {},
+    header: "Visual Dictionary"
+  })
 
   it("Should render self", () => {
-    const { container, queryByText } = render(<App {...props} />)
-    const header = queryByText(props.header)
+    const { container, queryByText } = render(<App {...createProps()} />)
+    const header = queryByText(createProps().header)
 
     expect(container.firstChild).toMatchSnapshot()
-    expect(header.innerHTML).toBe(props.header)
+    expect(header.innerHTML).toBe(createProps().header)
   })
 
   it("Should render a <SearchBox /> component", () => {
-    const { container } = render(<App {...props} />)
+    const { container } = render(<App {...createProps()} />)
     const searchBox = container.querySelectorAll(".SearchBox")
 
     expect(searchBox.length).toBe(1)
   })
 
   it("Should render a <SearchResults /> component", () => {
-    const { container } = render(<App {...props} />)
+    const { container } = render(<App {...createProps()} />)
     const results = container.querySelectorAll(".SearchResults")
 
     expect(results.length).toBe(1)
--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,28 +1,44 @@
 import React from "react"
-import { func, oneOfType, node, string } from "prop-types"
+import {
+  func,
+  oneOfType,
+  node,
+  string,
+  shape,
+  boolean,
+  arrayOf
+} from "prop-types"
 import SearchBox from "Components/App/SearchBox"
 import SearchResults from "Components/App/SearchResults"
 import { connect } from "react-redux"
 import searchWord from "Components/App/SearchBox/actions"
 
-const App = ({ header, searchWord }) => (
+export const App = ({ header, searchWord, SearchBoxStore }) => (
   <div>
     <h1>{header}</h1>
-    <SearchBox searchWord={searchWord} />
+    <SearchBox searchWord={searchWord} SearchBoxStore={SearchBoxStore} />
     <SearchResults />
   </div>
 )
 
 App.propTypes = {
   searchWord: func.isRequired,
-  header: oneOfType([node, string]).isRequired
+  header: oneOfType([node, string]).isRequired,
+  SearchBoxStore: shape({
+    isLoading: boolean,
+    results: arrayOf(shape({ shape }))
+  }).isRequired
 }
 
 const mapDispatchToProps = {
   searchWord
 }
 
+const mapStateToProps = state => ({
+  SearchBoxStore: state.SearchBox
+})
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App)
