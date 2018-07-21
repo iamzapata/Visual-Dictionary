d1235856c2f66619332f685b0dc24907cc743c5a
Andres Zapata <me@azapata.io>
2018-07-20 21:58:14 -0500

    Add mock lexical entry file

---
+++ src/Components/App/SearchResults/mocks.js
@@ -0,0 +1,19 @@
+const entrySample = {
+  text: "privacy",
+  entries: [],
+  language: "es",
+  lexicalCategory: "Noun",
+  pronunciations: [
+    {
+      audioFile: "http://audio.oxforddictionaries.com/en/mp3/privacy_gb_1.mp3",
+      phoneticSpelling: "prɪvəsi",
+      dialects: ["British English"]
+    },
+    {
+      phoneticSpelling: "prʌɪvəsi",
+      dialects: ["British English"]
+    }
+  ]
+}
+
+export default entrySample
