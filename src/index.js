039d7397948a520027d441fd516e7304f6c4cf56
Andres Zapata <me@azapata.io>
2018-07-20 20:42:00 -0500

    Remove extra forward slash

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import store from "./store"
-import App from "Components/App/"
+import App from "Components/App"
 
 ReactDOM.render(
   <Provider store={store}>
