7056977e2d9eeb993c30f07a6cb6622b6839916d
Andres Zapata <me@azapata.io>
2018-08-12 19:26:54 -0500

    Normalize entries in getLexicalEntries selector

--- src/Components/App/reducers/selectors.js
+++ src/Components/App/reducers/selectors.js
@@ -2,17 +2,30 @@ import { createSelector } from "reselect"
 import hydrateWithIds from "utils/hydrateWithId"
 import shortId from "shortid"
 
+const normalizeDefinitions = entries => {
+  return entries.map(en => ({
+    id: en.id,
+    etymologies: en.etymologies,
+    senses: en.senses.slice(0, 4)
+  }))
+}
+
 const getLexicalEntries = state => state.SearchStore.results
 
-const lexicalEntriesSelector = createSelector([getLexicalEntries], entries => {
-  if (entries.length > 0) {
-    const lexicalEntries = entries[0].lexicalEntries
-    return lexicalEntries.map(lexicalEntry => ({
-      ...lexicalEntry,
-      id: shortId.generate(),
-      pronunciations: hydrateWithIds(lexicalEntry.pronunciations),
-      entries: hydrateWithIds(lexicalEntry.entries)
-    }))
+const lexicalEntriesSelector = createSelector([getLexicalEntries], results => {
+  if (results.length > 0) {
+    const lexicalEntries = results[0].lexicalEntries
+    return lexicalEntries
+      .map(lexicalEntry => ({
+        ...lexicalEntry,
+        id: shortId.generate(),
+        pronunciations: hydrateWithIds(lexicalEntry.pronunciations),
+        entries: hydrateWithIds(lexicalEntry.entries)
+      }))
+      .map(results => ({
+        ...results,
+        entries: normalizeDefinitions(results.entries)
+      }))
   }
   return []
 })
